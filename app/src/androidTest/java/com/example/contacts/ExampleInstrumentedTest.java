f6e725c56d9ec30b84cc1cec14bee0cc61665593
Rabbit <xe72@yandex.ru>
2019-02-24 22:44:43 +0300

    Урок 41. Используем LayoutInflater для создания списка

--- app/src/androidTest/java/com/example/contacts/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/example/contacts/ExampleInstrumentedTest.java
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getTargetContext();
 
-        assertEquals("com.example.contacts", appContext.getPackageName());
+        assertEquals("com.example.contact", appContext.getPackageName());
     }
 }
--- app/src/main/java/com/example/contacts/MainActivity.java
+++ app/src/main/java/com/example/contacts/MainActivity.java
@@ -2,6 +2,13 @@ package com.example.contacts;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +16,29 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        List<Contact> phoneBook = createContacts();
+
+        LinearLayout linLayout = findViewById(R.id.linLayout);
+
+        LayoutInflater lnInflater = getLayoutInflater();
+
+        for (Contact c : phoneBook) {
+            View item = lnInflater.inflate(R.layout.contact, linLayout, false);
+            TextView tvName = item.findViewById(R.id.tvName);
+            tvName.setText(c.getName());
+            TextView tvNumber = item.findViewById(R.id.tvNumber);
+            tvNumber.setText(c.getPhone());
+            item.getLayoutParams().width = LinearLayout.LayoutParams.MATCH_PARENT;
+            linLayout.addView(item);
+        }
+    }
+
+    private List<Contact> createContacts() {
+        List<Contact> phoneBook = new ArrayList<>();
+        phoneBook.add(new Contact("Иван", "1234"));
+        phoneBook.add(new Contact("Илья", "4321"));
+        phoneBook.add(new Contact("Антон", "3421"));
+        return phoneBook;
     }
 }
