2ee266be486545df7c6948646c2c9fab30f05176
Rabbit <xe72@yandex.ru>
2019-03-06 21:23:34 +0300

    Список - ListView, SQLite Своя бд контактов

--- app/src/main/java/com/example/contacts/ContactAdapter.java
+++ app/src/main/java/com/example/contacts/ContactAdapter.java
@@ -1,11 +1,15 @@
 package com.example.contacts;
 
 import android.content.Context;
+import android.content.Intent;
+import android.media.Image;
+import android.net.Uri;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
-import android.widget.LinearLayout;
+import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.TextView;
 
 import java.util.List;
@@ -22,12 +26,20 @@ public class ContactAdapter extends ArrayAdapter<Contact> {
             convertView = LayoutInflater.from(getContext()).inflate(R.layout.contact, null);
         }
 
-        Contact contact = getItem(position);
+        final Contact contact = getItem(position);
         TextView tvName = convertView.findViewById(R.id.tvName);
         tvName.setText(contact.getName());
         TextView tvNumber = convertView.findViewById(R.id.tvNumber);
         tvNumber.setText(contact.getPhone());
-//        convertView.getLayoutParams().width = LinearLayout.LayoutParams.MATCH_PARENT;
+        ImageButton call = convertView.findViewById(R.id.call);
+        call.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String call = "tel:" + contact.getPhone();
+                Intent intent = new Intent(Intent.ACTION_CALL, Uri.parse(call));
+                getContext().startActivity(intent);
+            }
+        });
         return convertView;
     }
 }
