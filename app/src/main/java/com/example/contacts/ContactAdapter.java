0d9e51c1580adbb895ddbac6c2b497c7fe92e88f
Rabbit <xe72@yandex.ru>
2019-03-17 15:50:14 +0300

    Звонилка - переход на RecyclerView

--- app/src/main/java/com/example/contacts/ContactAdapter.java
+++ app/src/main/java/com/example/contacts/ContactAdapter.java
@@ -2,65 +2,67 @@ package com.example.contacts;
 
 import android.content.Context;
 import android.content.Intent;
-import android.media.Image;
 import android.net.Uri;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
-import android.widget.Button;
-import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import java.util.List;
 
-public class ContactAdapter extends RecyclerView.Adapter {
+public class ContactAdapter extends RecyclerView.Adapter<ContactAdapter.ViewHolder> {
+    private Context context;
+    private List<Contact> phoneBook;
 
-    public ContactAdapter(Context context, List<Contact> objects) {
-        super(context, objects);
-    }
-
-    @Override
-    public View getView(int position, View convertView, ViewGroup parent) {
-        if(convertView == null){
-            convertView = LayoutInflater.from(getContext()).inflate(R.layout.contact, null);
-        }
-
-        final Contact contact = getItem(position);
-        TextView tvName = convertView.findViewById(R.id.tvName);
-        tvName.setText(contact.getName());
-        TextView tvNumber = convertView.findViewById(R.id.tvNumber);
-        tvNumber.setText(contact.getPhone());
-        ImageView call = convertView.findViewById(R.id.call);
-        if (call != null) {
-            call.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    String call = "tel:" + contact.getPhone();
-                    Intent intent = new Intent(Intent.ACTION_CALL, Uri.parse(call));
-                    getContext().startActivity(intent);
-                }
-            });
-        }
-        return convertView;
+    public ContactAdapter(Context context, List<Contact> phonebook) {
+        this.phoneBook = phonebook;
+        this.context = context;
     }
 
     @NonNull
     @Override
-    public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
-        return null;
+    public ContactAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int viewType) {
+        View view = LayoutInflater.from(context).inflate(R.layout.contact, null);
+        final ViewHolder viewHolder = new ViewHolder(view);
+        viewHolder.call.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int adapterPosition = viewHolder.getAdapterPosition();
+                if (adapterPosition != RecyclerView.NO_POSITION) {
+                    String call = "tel:" + phoneBook.get(adapterPosition).getPhone();
+                    Intent intent = new Intent(Intent.ACTION_CALL, Uri.parse(call));
+                    context.startActivity(intent);
+                }
+            }
+        });
+        return viewHolder;
     }
 
     @Override
-    public void onBindViewHolder(@NonNull RecyclerView.ViewHolder viewHolder, int i) {
-
+    public void onBindViewHolder(@NonNull ContactAdapter.ViewHolder viewHolder, int position) {
+        Contact contact = phoneBook.get(position);
+        viewHolder.tvName.setText(contact.getName());
+        viewHolder.tvNumber.setText(contact.getPhone());
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return phoneBook.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        final TextView tvName;
+        final TextView tvNumber;
+        final ImageView call;
+
+        public ViewHolder(@NonNull View view) {
+            super(view);
+            tvName = view.findViewById(R.id.tvName);
+            tvNumber = view.findViewById(R.id.tvNumber);
+            call = view.findViewById(R.id.call);
+        }
     }
 }
